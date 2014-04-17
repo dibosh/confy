11c50aa930e51a406877409be737b034dc924094
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-04-18 03:19:30 +0530

    Finished retrieving a team

--- apps/api/teams/index.js
+++ apps/api/teams/index.js
@@ -1,3 +1,25 @@
 module.exports = function (app, db) {
 
+  // Team param
+  app.param('team', function (req, res, next, team) {
+    var id = 'orgs/' + req.org.name.toLowerCase() + '/teams/' + team;
+
+    db.get(id, function (err, body) {
+      if (err) return next(err);
+
+      if (body) {
+        req.team = body;
+        return next();
+      }
+
+      return app.errors.notfound(res);
+    });
+  });
+
+  // Retrieve a team
+  app.get('/orgs/:org/teams/:team', app.auth, app.auth.owner, function (req, res, next) {
+    app.utils.shield(req.team, ['_rev']);
+    res.json(req.team);
+  });
+
 };

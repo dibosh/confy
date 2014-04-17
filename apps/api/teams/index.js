87982fa35627bd0e8e952bc203e7d9d8626684ed
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-04-18 03:35:19 +0530

    Finished creating a team

--- apps/api/teams/index.js
+++ apps/api/teams/index.js
@@ -22,4 +22,47 @@ module.exports = function (app, db) {
     res.json(req.team);
   });
 
+  // Create a team
+  app.post('/orgs/:org/teams', app.auth, app.auth.owner, function (req, res, next) {
+    app.utils.permit(req, ['name', 'description']);
+
+    // Check for required params
+    var errs = app.utils.need(req, ['name', 'description']);
+
+    var name = req.body.name;
+
+    if (name && name.match(/[a-z0-9]*/i)[0] != name) {
+      errs.push({ field: 'name', code: 'invalid' });
+    }
+
+    if (errs.length > 0) {
+      return app.errors.validation(res, errs);
+    }
+
+    var orgLowerName = req.org.name.toLowerCase()
+      , lowerName = name.toLowerCase();
+
+    // Search for existing teamname
+    db.view('teams', 'name', {key: [orgLowerName + '/' + lowerName]}, function (err, body) {
+      if (err) return next(err);
+
+      if (body.rows.length > 0) {
+        return app.errors.validation(res, [{ field: 'name', code: 'already_exists' }]);
+      }
+
+      req.body.type = 'team';
+      req.body.members = [];
+
+      // Insert team
+      db.insert(req.body, 'orgs/' + orgLowerName + '/teams/' + lowerName, function (err, body) {
+        if (err) return next(err);
+
+        if (body.ok) {
+          req.body._id = body.id;
+          res.status(201);
+          res.json(req.body);
+        }
+      });
+    });
+  });
 };

6ca9b2c428cd1fa70862fc52dfebe014c21916bc
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-04-18 04:11:26 +0530

    Finished retrieving a project

--- apps/api/projects/index.js
+++ apps/api/projects/index.js
@@ -1,3 +1,24 @@
 module.exports = function (app, db) {
 
+  // Project param
+  app.param('project', function (req, res, next, project) {
+    var id = 'orgs/' + req.org.name.toLowerCase() + '/projects/' + project;
+
+    db.get(id, function (err, body) {
+      if (err) return next(err);
+
+      if (body) {
+        req.project = body;
+        return next();
+      }
+
+      return app.errors.notfound(res);
+    });
+  });
+
+  // Retrieve a project
+  app.get('/orgs/:org/projects/:project', app.auth, app.auth.owner, function (req, res, next) {
+    app.utils.shield(req.project, ['_rev']);
+    res.json(req.project);
+  });
 };

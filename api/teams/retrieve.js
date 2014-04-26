a8a5c4b4c668754cca0865daf8c51d5ef351a472
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-04-27 02:14:32 +0530

    Update the api blueprint after fixing teams

--- api/teams/retrieve.js
+++ api/teams/retrieve.js
@@ -2,9 +2,7 @@ module.exports = function (app, db) {
 
   // Retrieve a team
   app.get('/orgs/:org/teams/:team', app.auth.user, function (req, res, next) {
-    var user = req.user.username;
-
-    if (req.org.owner != user && req.team.users[user] === undefined) {
+    if (req.team.users[req.user.username] === undefined) {
       return app.errors.notfound(res);
     }
 

777340da7f01d33e9c61094b8c64a83dee06881a
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-04-27 02:40:30 +0530

    Listing teams will list all teams where user is a member

--- api/teams/list.js
+++ api/teams/list.js
@@ -1,13 +1,14 @@
 module.exports = function (app, db) {
 
   // List all teams
-  app.get('/orgs/:org/teams', app.auth.owner, function (req, res, next) {
-    db.view('teams', 'org', {keys: [req.org.name.toLowerCase()]}, function (err, body) {
+  app.get('/orgs/:org/teams', app.auth.user, function (req, res, next) {
+    db.view('teams', 'user', {keys: [req.org.name.toLowerCase() + '/' + req.user.username]}, function (err, body) {
       if (err) return next(err);
 
       if (body.rows) {
         body = body.rows.map(function (row) {
           app.utils.shield(row.value, ['_rev']);
+          row.value.users = Object.keys(row.value.users);
           return row.value;
         });
 

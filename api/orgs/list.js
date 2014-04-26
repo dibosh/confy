c027e77ce9f110e3ba3a200ae23cd69be44a27b5
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-04-26 20:46:32 +0530

    Now orgs list will list orgs with user as member too

--- api/orgs/list.js
+++ api/orgs/list.js
@@ -2,12 +2,12 @@ module.exports = function (app, db) {
 
   // List all orgs
   app.get('/orgs', app.auth.user, function (req, res, next) {
-    db.view('orgs', 'owner', {keys: [req.user.username]}, function (err, body) {
+    db.view('orgs', 'user', {keys: [req.user.username]}, function (err, body) {
       if (err) return next(err);
 
       if (body.rows) {
         body = body.rows.map(function (row) {
-          app.utils.shield(row.value, ['_rev']);
+          app.utils.shield(row.value, ['users', '_rev']);
           return row.value;
         });
 

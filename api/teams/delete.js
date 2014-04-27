0c932e22505730422487cc9da4ec386b1e1a84cc
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-04-28 00:15:06 +0530

    Modify user list in projects and org when team is deleted

--- api/teams/delete.js
+++ api/teams/delete.js
@@ -2,12 +2,46 @@ module.exports = function (app, db) {
 
   // Delete a team
   app.delete('/orgs/:org/teams/:team', app.auth.owner, function (req, res, next) {
-    db.destroy(req.team._id, req.team._rev, function (err, body) {
+    var org = req.org.name.toLowerCase()
+      , team = req.team.name.toLowerCase();
+
+    // Update projects
+    db.view('projects', 'team', {keys:[org + '/' + team]}, function (err, body) {
       if (err) return next(err);
 
-      if (body.ok) {
+      var projects = body.rows.map(function (row) {
+        delete row.value.teams[team];
+
+        Object.keys(req.team.users).forEach(function (user) {
+          row.value.users[user]--;
+
+          if (row.value.users[user] === 0) {
+            delete row.value.users[user];
+          }
+        });
+
+        return row.value;
+      });
+
+      // Update data
+      req.team._deleted = true;
+
+      Object.keys(req.team.users).forEach(function (user) {
+        req.org.users[user]--;
+
+        if (req.org.users[user] === 0) {
+          delete req.org.users[user];
+        }
+      });
+
+      projects.push(req.org);
+      projects.push(req.team);
+
+      db.bulk({docs: projects}, {all_or_nothing: true}, function (err, body) {
+        if (err) return next(err);
+
         res.send(204);
-      } else next();
+      });
     });
   });
 };
